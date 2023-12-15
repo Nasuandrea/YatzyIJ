7dc49d3ba34631f2719d42eba3ee0ad9a7580dc3
nasua <acollazocacho@gmail.com>
2023-12-15 11:38:40 +0100

    refactor(Main , YatzyTest): refactorización funcion ones y caso test correspondiente cmabio de parametros por var args y simplificación de bucle for

--- src/main/java/org/edu/iesTeis/Main.java
+++ src/main/java/org/edu/iesTeis/Main.java
@@ -20,16 +20,16 @@ public class Main {
         return 0;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1)
-            sum++;
+    public static int ones(int... dice) {
+        int suma = 0;
 
-        return sum;
+        for (int dado : dice) {
+            if (dado == 1) {
+                suma++;
+            }
+        }
+
+        return suma;
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
