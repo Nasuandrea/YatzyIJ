7e53cd900b55f0a22e9b7577009a22dbfc401f37
nasua <acollazocacho@gmail.com>
2023-12-15 11:03:41 +0100

    actualizacion a jUnit

--- test/java/YatzyTest.java
+++ test/java/YatzyTest.java
@@ -1,5 +1,7 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.example.Yatzy;
+
 
 public class YatzyTest {
 
